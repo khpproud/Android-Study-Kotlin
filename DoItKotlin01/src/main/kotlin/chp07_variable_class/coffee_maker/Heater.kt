7e2e522359c0aea1b58b73cfbb4b9f81437f66a1
khpproud <proudkhp1@gmail.com>
2019-07-10 22:19:16 +0900

    unnecessary delete

--- DoItKotlin01/src/main/kotlin/chp07_variable_class/coffee_maker/Heater.kt
+++
@@ -1,8 +0,0 @@
-package chp07_variable_class.coffee_maker
-
-// Heater 인터페이스
-interface Heater {
-    fun on()
-    fun off()
-    fun isHot(): Boolean
-}
--- DoItKotlin01/src/main/kotlin/chp07_variable_class/coffee_maker/Pump.kt
+++
@@ -1,5 +0,0 @@
-package chp07_variable_class.coffee_maker
-
-interface Pump {
-    fun pump()
-}
